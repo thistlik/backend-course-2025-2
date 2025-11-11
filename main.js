d8786e8fdabd6b048984ef5eb078dbd75a8577db
Юрій Шайда bc2025-2 <shaydayur@gmail.com>
2025-11-11 05:31:46 +0200

    Edited main.js for part 3

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
+// варіант 5 — Міжнвродні резерви
 
+// дата, формат YYYYMMDD)
+const date = "20220201";
+const baseURL = "https://bank.gov.ua/";
+const fullURL = new URL("NBUStatService/v1/statdirectory/res", baseURL);
+fullURL.searchParams.append("date", date);
+fullURL.searchParams.append("json", "");
+
+console.log("Сформований URL:");
+console.log(fullURL.href);
